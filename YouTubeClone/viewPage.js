fcd5b76c5e3f10ea4d97dfd01174e4d0dfdcefd8
Swati Kumari <swatikrionly@gmail.com>
2025-06-30 22:17:48 +0530

    callback

--- YouTubeClone/viewPage.js
+++ YouTubeClone/viewPage.js
@@ -3,6 +3,5 @@ console.log("searchQueryStr", searchQueryStr);
 const res = searchQueryStr.split("=");
 console.log(res);
 const videoId = res[1];
-
 const iframe = document.getElementsByTagName("iframe")[0];
 iframe.setAttribute("src", `https://www.youtube.com/embed/${videoId}`);
